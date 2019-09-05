f524891e8d83de09cf62403ddf505d12f48583bf
alex-kar <karyakinnn@gmail.com>
2019-09-05 11:03:06 +0400

    jsf refactoring

--- ejb-module/src/main/resources/db/populateDB.sql
+++ ejb-module/src/main/resources/db/populateDB.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (name) VALUES ('ivanov ivan'), ('boris borisov');
 
-INSERT INTO items (name, price) VALUES ('sony', 80), ('samsung', 60), ('philips', 40), ('lg', 20);
+INSERT INTO items (name, price) VALUES ('sony', 80), ('samsung', 60), ('philips', 40), ('lg', 20), ('fly', 10), ('nokia', 30), ('huawei', 50), ('iphone', 70);
 
-INSERT INTO warehouse (item_id, count) VALUES (1, 10), (2, 10), (3, 10), (4, 10);
+INSERT INTO warehouse (item_id, count) VALUES (1, 10), (2, 10), (3, 10), (4, 10), (5, 10), (6, 10), (7, 10), (8, 10);
 
 INSERT INTO orders (user_id) VALUES (1);
 
